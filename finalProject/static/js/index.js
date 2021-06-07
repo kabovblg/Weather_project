004f9c6c3f286a7fa3d461eda88e6a46ab8e11c1
Blagovest Kabov <kabov.blg@gmail.com>
2021-06-07 17:35:58 +0300

    fetching

--- finalProject/static/js/index.js
+++ finalProject/static/js/index.js
@@ -6,23 +6,27 @@ let lat;
 let lon;
 
 function showPosition(position) {
-    return console.log(position.coords);
-     let lat = position.coords.latitude;
-    let lon = position.coords.longtitude;
-  }
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+  
 
 
 //fetching data
 fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=1bfb67ac210a55c22a6fb24f3051b111`)
     .then( (response) => {
-        if (response.status >= 200 && response.status < 300) {
+        if (response.status < 200 && response.status > 300) {
             console.log("There is a problem!");
           }
-
         response.json().then( (data) => {
+          let today_icon = document.getElementById('today_icon');
+          today_icon.scr = data.weather[1];
+
+          
         console.log(data);
         })
     })
     .catch((err) => {
         console.log("Тhe following error occured!");
       });
+    
+    }
