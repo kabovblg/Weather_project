8275cc4995164ed53f441d8e39f12912e2fdea71
Blagovest Kabov <kabov.blg@gmail.com>
2021-06-10 14:43:09 +0300

    .

--- finalProject/static/js/index.js
+++ finalProject/static/js/index.js
@@ -4,6 +4,9 @@ window.onload = navigator.geolocation.getCurrentPosition(showPosition);
 
 let lat;
 let lon;
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let d = new Date();
+let n = d.getDay();
 
 function showPosition(position) {
     lat = position.coords.latitude;
@@ -16,6 +19,8 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&app
             console.log("There is a problem!");
           }
         response.json().then( (data) => {
+          document.getElementById('title').innerHTML = days[n];
+
           let today_icon_url = data.weather[0].icon;
           let today_icon = document.getElementById('today_icon');
           today_icon.src = `http://openweathermap.org/img/wn/${today_icon_url}.png`
@@ -61,24 +66,39 @@ fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exc
           console.log(data);
           
           for(let i=0; i<3; i++){
+            if(n+i<7){
+                document.getElementById('threeDays_day'+i).innerHTML = days[n+i];
+              }else {
+                document.getElementById('threeDays_day'+i).innerHTML = days[n-7+i];
+              }
+          
             document.getElementById('threeDays_icon'+i).src = `http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}.png`;
             document.getElementById('threeDays_temp'+i).innerHTML = (data.daily[i].temp.day - 273.15).toFixed(0) + "°";
             document.getElementById('threeDays_hum'+i).innerHTML = data.daily[i].humidity  + "%";
             document.getElementById('threeDays_rain'+i);
             if(data.daily[i].rain){
-              ('threeDays_rain'+i).innerHTML = data.daily[i].rain + "l/m²";
+              document.getElementById('threeDays_rain'+i).innerHTML = data.daily[i].rain + " l/m²";
             } else {
-              ('threeDays_rain'+i).innerHTML = "0%";
-              }
+              document.getElementById('threeDays_rain'+i).innerHTML = "0%";
+            }
             document.getElementById('threeDays_ws'+i).innerHTML = data.daily[i].wind_speed  + "m/s";
             document.getElementById('threeDays_clouds'+i).innerHTML = data.daily[i].clouds  + "%";
             }
 
             for(let i=0; i<7; i++){
+              if(n+i<7){
+                document.getElementById('sevenDays_day'+i).innerHTML = days[n+i];
+              }else {
+                document.getElementById('sevenDays_day'+i).innerHTML = days[n-7+i];
+              }
               document.getElementById('sevenDays_icon'+i).src = `http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}.png`;
               document.getElementById('sevenDays_temp'+i).innerHTML = (data.daily[i].temp.day - 273.15).toFixed(0) + "°";
               document.getElementById('sevenDays_hum'+i).innerHTML = data.daily[i].humidity  + "%";
-              // document.getElementById('sevenDays_rain'+i);
+              if(data.daily[i].rain){
+                document.getElementById('sevenDays_rain'+i).innerHTML = data.daily[i].rain + " l/m²";
+              } else {
+                document.getElementById('sevenDays_rain'+i).innerHTML = "0%";
+              }
               document.getElementById('sevenDays_ws'+i).innerHTML = data.daily[i].wind_speed.toFixed(1)  + "m/s";
               document.getElementById('sevenDays_clouds'+i).innerHTML = data.daily[i].clouds  + "%";
               }
